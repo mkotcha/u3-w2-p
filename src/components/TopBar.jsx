797141202cf0fa4679f7a437457b89caedfd0cff
Matteo Falasca <matteo.falasca@emmek.org>
2023-09-15 10:01:34 +0200

    topBar

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,20 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+const TopBar = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">EpiWeather</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Favourites</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default TopBar;
